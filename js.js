f3ec87693fe0277480383fb0e0d364f79bf35f37
Isaias <lawicki76@gmail.com>
2021-10-19 21:09:07 -0300

    Change the animations and another interfaz

--- js.js
+++ js.js
@@ -35,3 +35,18 @@ for(var i = 0; i < imagen.length; i++){
 
 
 })
+
+var icon = document.getElementById("icon");
+var icon1 = document.getElementById("a");
+var icon2 = document.getElementById("b");
+var icon3 = document.getElementById("c");
+var nav = document.getElementById('nav');
+var blue = document.getElementById("blue");
+
+icon.addEventListener('click', function() {
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
+  nav.classList.toggle('show');
+  blue.classList.toggle('slide');
+});
