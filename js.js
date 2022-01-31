de7fae17196d1640f58ea0a580fa217e3a930e96
Isaias <lawicki76@gmail.com>
2022-01-30 22:13:27 -0300

    Start the Responsive design

--- js.js
+++ js.js
@@ -1,4 +1,3 @@
-
 var icon = document.getElementById("icon");
 var icon1 = document.getElementById("a");
 var icon2 = document.getElementById("b");
@@ -12,4 +11,16 @@ icon.addEventListener('click', function() {
   icon3.classList.toggle('b');
   nav.classList.toggle('show');
   blue.classList.toggle('slide');
+});
+
+var typed = new Typed('.element', {
+  strings: ["Somos una productora audiovisual de <b>Contenido productivo</b>.", "Productora de publicidad para contenido <b>Social media</b>."],
+  typeSpeed: 60,
+  backSpeed: 25,
+  startDelay: 300,
+  loop: true,
+  loopCount: false,
+  showCursor: true,
+  cursorChar: '|',
+  contentType: 'html'
 });
