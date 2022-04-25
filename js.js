d3f71f6aa80e20e0b389743ff879f78879d5ba08
Isaias <lawicki76@gmail.com>
2022-04-24 21:19:22 -0300

    Valid the form with JavaScript

--- js.js
+++ js.js
@@ -25,3 +25,17 @@ var typed = new Typed('.element', {
   cursorChar: '|',
   contentType: 'html',
 });
+
+const $form = document.querySelector('#form') 
+        const $buttonMailto = document.querySelector('#trucazo') 
+
+        $form.addEventListener('submit', handleSubmit)
+
+        function handleSubmit(event){
+        event.preventDefault()
+        const form = new FormData(this)
+        console.log(form.get('name'))
+        $buttonMailto.setAttribute('href', `mailto:ninety9@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+        $buttonMailto.click()
+        
+    }
